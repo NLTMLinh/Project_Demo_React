d665c106171f964e854675e55597b7881369aced
MyLinhIT <1612334@student.hcmus.edu.vn>
2019-05-16 21:13:23 +0700

    Thêm hàm fetchEmloyeesLoading trong actions/actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,9 +1,55 @@
-export const LogIn = data => ({
-    type: 'LOG_IN',
-    data
-});
+import {LOG_IN,LOG_OUT,SIGN_IN,DELETE_USER,EDIT_USER,FETCH_EMPLOYEES_ERROR,FETCH_EMPLOYEES_SUCCESS,FETCH_EMPLOYEES_LOADING} from "../constants/ActionTypes";
 
-export const LogOut = data => ({
-    type: 'LOG_OUT',
-    data
-});
+export function LogIn(isLoggedIn,name){
+    return{
+        type: LOG_IN,
+        isLoggedIn,
+        name
+    }
+};
+
+export function LogOut(isLoggedIn){
+    return{
+        type: LOG_OUT,
+        isLoggedIn
+    }
+};
+
+export function SignIn(user) {
+    return{
+        type: SIGN_IN,
+        user
+    }
+
+}
+export function DeleteUser(id) {
+    return{
+        type: DELETE_USER,
+        id
+    }
+}
+export function EditUser(user) {
+    return{
+        type: EDIT_USER,
+        user
+    }
+}
+export function FetchEmployeesSuccess(employees) {
+    return {
+        type: FETCH_EMPLOYEES_SUCCESS,
+        employees
+    }
+}
+export function FetchEmployeesError(error) {
+    return{
+        type: FETCH_EMPLOYEES_ERROR,
+        error
+    }
+
+}
+export function FetchEmployeesLoading() {
+    return{
+        type: FETCH_EMPLOYEES_LOADING
+    }
+
+}
